1a95490c5a3095ab3a9ec5063821e3b346701534
xcitic <sami@ecompx.com>
2019-05-10 12:55:40 +0200

    Vuex Store Module Based

--- frontend/app.js
+++ frontend/app.js
@@ -2,19 +2,21 @@
 require('./bootstrap');
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 
 // External Modules
 import axios from 'axios';
 import Vuetify from 'vuetify';
 
-
 // Internal Modules
 import App from './App';
 import routes from '@/router';
+import stores from '@/stores';
 
 
 // Vue Plugins
 Vue.use(VueRouter);
+Vue.use(Vuex);
 Vue.use(Vuetify);
 
 // Routing
@@ -23,12 +25,18 @@ const router = new VueRouter({
   routes
 });
 
+// Vuex Store
+const store = new Vuex.Store({
+    stores
+});
+
 
 
 
 const app = new Vue({
     el: '#app',
     router,
+    store,
     components: {
       App
     },
---
+++ frontend/stores/index.js
@@ -0,0 +1,13 @@
+// Import different Stores as Seperate Modules with Namespaces
+
+
+const stores = () => {
+    return {
+      modules: {
+
+      }
+    }
+  }
+
+
+export default stores();
