4b5db1a91c6c3c5e3c1eb3ec9ea0cc78cabb4b1c
xcitic <sami@ecompx.com>
2019-05-10 14:07:38 +0200

    Force API response to JSON

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => ['JsonResponse']], function() {
+  Route::post('/login', 'Api\AuthController@login');
+  Route::post('/register', 'Api\AuthController@register');
+});
