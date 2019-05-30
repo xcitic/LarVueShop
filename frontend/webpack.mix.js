143889eb6f6500dd3932ce2666e648eada94ddc4
xcitic <sami@ecompx.com>
2019-05-30 20:41:24 +0200

    Refactoring for deployment with dockerized DB

---
+++ frontend/webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.webpackConfig({
+  resolve: {
+    extensions: ['.js', '.vue'],
+    alias: {
+      '@': __dirname + ''
+    },
+  }
+});
+
+mix.js('./app.js', '../backend/public/js/app.js')
+    .sass('./assets/sass/app.scss', '../backend/public/css/app.css')
+    .version();
